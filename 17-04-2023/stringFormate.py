aa2dbc293c4dbc00acd7d9b70f1596485a4d7ff0
nahidfarazi <nahidfarazi19@gmail.com>
2023-04-30 10:01:43 +0600

    Arry-start

--- 17-04-2023/stringFormate.py
+++ 17-04-2023/stringFormate.py
@@ -1,3 +1,6 @@
-num1 = 20
-num2 = 30
-print(f"thus is my total number : {num1+num2}")
+# num1 = 20
+# num2 = 30
+# print(f"thus is my total number : {num1+num2}")
+
+userName = 'Nahid'
+print(f'my name is {userName}')
---
+++ Zulkarnine/first.py
@@ -0,0 +1,6 @@
+def add_tow_value(first, second):
+    return first + second
+
+
+result = add_tow_value(20, 30)
+print(result)
